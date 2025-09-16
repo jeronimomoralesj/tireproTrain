2eccfe474c8f485d80ccd677927e5efff16b927b
Jeronimo <jeronimo@Jeronimos-MacBook-Pro.local>
2025-09-16 00:21:33 -0500

    first fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,15 +6,21 @@ if (!uri) throw new Error("Please add your MONGODB_URI to .env.local");
 
 const options = {};
 
-let client = new MongoClient(uri, options);
+// Declare global type for MongoDB client promise
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+const client = new MongoClient(uri, options);
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // In dev mode, use a global variable so we don't create a new connection on every reload
-  if (!(global as any)._mongoClientPromise) {
-    (global as any)._mongoClientPromise = client.connect();
+  if (!global._mongoClientPromise) {
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
   // In production, just connect once
   clientPromise = client.connect();
